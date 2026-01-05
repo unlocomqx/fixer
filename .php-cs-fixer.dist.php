83da8d16ceaa90f0decc1ebd62f73be35f1dbec2
Tuni-Soft <amxmodx.unloco@gmail.com>
2026-01-05 07:21:09 +0100

    Update PHP CS Fixer config rules

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -1,12 +1,26 @@
 <?php
 
-$config = new PrestaShop\CodingStandards\CsFixer\Config();
+$config = new PhpCsFixer\Config('PrestaShop coding standard');
+$config->setRiskyAllowed(true);
 
-$config->setRules(array_merge($config->getRules(), [
-    'trailing_comma_in_multiline' => [
-        'elements' => ['arrays', 'array_destructuring', 'match'],
-    ],
-]));
+$config->setRules([
+    '@Symfony' => true,
+    'concat_space' => ['spacing' => 'one'],
+    'cast_spaces' => ['space' => 'single'],
+    'error_suppression' => ['mute_deprecation_error' => false, 'noise_remaining_usages' => false, 'noise_remaining_usages_exclude' => []],
+    'function_to_constant' => false,
+    'visibility_required' => ['elements' => ['property', 'method']],
+    'no_alias_functions' => false,
+    'phpdoc_summary' => false,
+    'phpdoc_align' => ['align' => 'left'],
+    'protected_to_private' => false,
+    'psr_autoloading' => false,
+    'self_accessor' => false,
+    'yoda_style' => false,
+    'non_printable_character' => true,
+    'no_superfluous_phpdoc_tags' => false,
+    'trailing_comma_in_multiline' => ['elements' => ['arrays', 'array_destructuring', 'match']],
+]);
 
 /** @var \Symfony\Component\Finder\Finder $finder */
 $finder = $config->setUsingCache(true)->getFinder();
